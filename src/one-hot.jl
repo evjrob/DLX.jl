b7961cdee925dcdbd7f4ae71e8d09a5d9c28cf5d
mortem.dei@gmail.com <mortem.dei@gmail.com>
2017-10-18 23:01:27 +0200

    added additional methods to get_cover

--- src/one-hot.jl
+++ src/one-hot.jl
@@ -24,8 +24,8 @@ end
 
 
 """
-ex::one_hot type with x and y arrays of strings and M matrix
-returns 
+`ex::one_hot` type with x and y arrays of strings and M matrix
+returns the indicies of the arrays in `ex` which cover `x`
 """
 function solve_cover(ex::one_hot)
     f = (ri, ci) -> (ex.M[ri, ci] == 1)
@@ -33,11 +33,34 @@ function solve_cover(ex::one_hot)
     return (search_root(root))
 end
 
-function get_cover(ex::one_hot)
+"""
+Returns the actual cover sets for `ex::one_hot`
+"""
+function get_cover(ex::one_hot)::Array{Array{Array{String, 1}, 1}, 1}
     cover_sets = solve_cover(ex)
     if length(cover_sets) >0
-        return ([ex.y[i] for i in cover_sets][1])
+        return get_cover(ex, cover_sets)
     else
-        return (Array{Array{String,1},1}())
+        return (Array{Array{Array{String, 1}, 1}, 1}())
     end
+end
+
+# depending on the number of responses return one or multiple
+
+function get_cover(ex::one_hote,
+                   sol::Array{Integer,1})::Array{Array{Array{String, 1}, 1}, 1}
+    return ([[ex.y[i] for i in sol][1]])
+end
+
+function get_cover(ex::one_hot,
+                   sol::Array{Array{Integer,1},1})::Array{Array{Array{String, 1}, 1}, 1}
+    covers = Array{Array{Array{String, 1}, 1}, 1}()
+    for covs in sol # iterate over each cover solution
+        sets = Array{Array{String, 1}, 1}()
+        for j in covs # iterate for sets in each solution
+            sets = vcat(sets, [ex.y[j]])
+        end
+        covers = vcat(covers, [sets])
+    end
+    return (covers)
 end
